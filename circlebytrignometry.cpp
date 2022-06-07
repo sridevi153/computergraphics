6d364e8e3af9d70637411c68ba42c3d9b514d906
sridevi153 <82940606+sridevi153@users.noreply.github.com>
2022-06-07 10:30:29 +0530

    Create circlebytrignometry.cpp

---
+++ circlebytrignometry.cpp
@@ -0,0 +1,39 @@
+#include<math.h>
+#include<stdlib.h>
+#include<glut.h>
+
+void myinit()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-100, 100, -100, 100);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void display()
+{
+	int xc = 0, yc = 0, r = 30;
+	float x, y;
+	glClearColor(1, 1, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1, 0, 1);
+	glBegin(GL_POINTS);
+	for (int i = 0; i <= 360; i += 1)
+	{
+		x = xc + r * cos(i);
+		y = yc + r * sin(i);
+		glVertex2f(x, y);
+	}
+	glEnd();
+	glFlush();
+}
+
+void main()
+{
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("circle");
+	myinit();
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
